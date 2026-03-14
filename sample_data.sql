278e17d28197d509c7425c7875c231a0d2c20d3e
vinit-30 <vinitkolhe17@gmail.com>
2026-03-14 18:09:12 +0530

    sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO customers VALUES
+(1,'Rahul','rahul@gmail.com','9876543210','Mumbai'),
+(2,'Priya','priya@gmail.com','9876543211','Pune'),
+(3,'Rohit','rohit@gmail.com','8876543210','Nagpur'),
+(4,'Riya','riya@gmail.com','7776543211','Chhatrapati Sambhajinagar');
+
+
+INSERT INTO products VALUES
+(101,'Laptop',60000,10),
+(102,'Headphones',2000,50),
+(103,'Laptop',60000,10),
+(104,'Headphones',2000,50);
+
+INSERT INTO orders VALUES
+(1001,1,'2026-03-14',62000);
